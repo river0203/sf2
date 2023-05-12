5c3c44de81a79e2aff94cfd5944622e01c1747ca
wonjae1267 <bjng1267@naver.com>
2023-05-12 13:53:32 +0900

    Update Blinking Script

--- sf2/Assets/Script/Blinking.cs
+++ sf2/Assets/Script/Blinking.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 public class Blinking: MonoBehaviour
 {
+    float time;
+
     void Update()
     {
+        if(time < 0.5f)
+        {
+            GetComponent<SpriteRenderer>().color=new Color(1,1,1,1-time);
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().color=new Color(1,1,1,time);
+            if(time>1f)
+            {
+                time=0;
+            }
+        }
+        time+=Time.deltaTime;
         
     }
 }
