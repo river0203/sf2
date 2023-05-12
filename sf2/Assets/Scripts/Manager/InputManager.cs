a8262b701f45a02362427e3a6596aa2dbe045e1e
river0203 <aspp3666@gmail.com>
2023-05-12 13:37:39 +0900

    FEAT_inputmanager

--- sf2/Assets/Scripts/Manager/InputManager.cs
+++ sf2/Assets/Scripts/Manager/InputManager.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Action PressButtonCheck;
+    //key 입력을 bool 값으로 확인
+    //확인된 정보를 다른 UI, character로 제공
+    
+    static void PressButton(bool ButtonKey) //Action 구현
     {
-        
+        if(Input.anyKeyDown)
+        {
+            Debug.Log("user press anykey");
+            ButtonKey = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake() 
     {
-        
+        PressButtonCheck += PressButton;
     }
+
 }
