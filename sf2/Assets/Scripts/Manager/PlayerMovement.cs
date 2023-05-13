cd36b46fe90e5cfda05893059aa30a13d023834d
river0203 <aspp3666@gmail.com>
2023-05-13 19:31:49 +0900

    FEAT : PlayerMovement

--- sf2/Assets/Scripts/Manager/PlayerMovement.cs
+++ sf2/Assets/Scripts/Manager/PlayerMovement.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+    //좌표를 통한 캐릭터 이동
 public class PlayerMovement : MonoBehaviour 
 {
-    private void Start() 
+    float moveX, moveY;
+    [SerializeField]
+    float maxSpeed = 5f;
+    
+    private void Update() 
     {
-        
+        moveX = Input.GetAxis("Horizontal") * maxSpeed * Time.deltaTime;
+        moveY = Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime;
+
+        transform.position = new Vector2(transform.position.x + moveX, transform.position.y + moveY);
     }
+
 }
