d3b1a409b19b4702d1016f81f4fd3c3ce76d1da5
river0203 <aspp3666@gmail.com>
2023-05-14 22:29:36 +0900

    FIX : player_movement, player_HP

--- sf2/Assets/Scripts/Manager/PlayerHP.cs
+++ sf2/Assets/Scripts/Manager/PlayerHP.cs
@@ -3,18 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class PlayerHP
+public class PlayerHP 
 {
+    public Action<bool> HpState;
     [SerializeField]
     float hp  = 100.0f;
-    public Action<bool> HpState;
 
     private void player_hp()
     {
-        if(player_hp == 0.0)
+        if(player_hp == 0.0f)
         {
             HpType = false;
         }
+        else if(player_hp != 0.0f)
+        {
+            HpType = true;
+        }
+    }
+
+    private void Start() 
+    {
+        HpState += HpType;
+    }
+    private void Update() 
+    {
+        
     }
 
 
--- sf2/Assets/Scripts/Manager/PlayerMovement.cs
+++ sf2/Assets/Scripts/Manager/PlayerMovement.cs
@@ -1,20 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-    //좌표를 통한 캐릭터 이동
+// 좌표를 통한 캐릭터 이동
+// 조건이 되었을 때 행동 차단 시간, HP
 public class PlayerMovement : MonoBehaviour 
 {
-    float moveX, moveY;
     [SerializeField]
     float maxSpeed = 5f;
+    float moveX, moveY;
     
     private void Update() 
+    {
+        player_move();
+        player_Nmove();
+    }
+
+    private void player_move()
     {
         moveX = Input.GetAxis("Horizontal") * maxSpeed * Time.deltaTime;
         moveY = Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime;
 
         transform.position = new Vector2(transform.position.x + moveX, transform.position.y + moveY);
+    }
+
+    private void player_Nmove()
+    {
+        // if bool 값을 time에서 불러옴
+        if (/*TimeBool_type == False*/)
+        {
+            maxSpeed = 0.0f;
+            Debug.Log("player stop");
+        }
+
+        else if(/*hp == 0*/)
+        {
+            maxSpeed = 0.0f;
+            Debug.Log("player stop");
+        }
     }
 
 }
