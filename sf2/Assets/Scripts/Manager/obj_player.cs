0b1e00f47fdd080032ff51cd65cfcb541bb43a4c
river0203 <aspp3666@gmail.com>
2023-05-14 22:28:53 +0900

    FEAT : player_object

---
+++ sf2/Assets/Scripts/Manager/obj_player.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+//player에 필요한 요소 관리, 통로 같은 역할
+public class obj_player
+{
+    private void Update() 
+    {
+        
+    }
+}
