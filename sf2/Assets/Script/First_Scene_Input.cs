86a280d4b1175512af19a11e6f5817d1d3d13224
wonjae1267 <bjng1267@naver.com>
2023-05-16 14:37:05 +0900

    FEAT : Game Scene Hp bar

--- sf2/Assets/Script/First_Scene_Input.cs
+++ sf2/Assets/Script/First_Scene_Input.cs
@@ -5,11 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class First_Scene_Input : MonoBehaviour
 {
+   
     void Update()
     {
-        if(Input.GetKey(KeyCode.Space)) //스페이스바를 입력 시 
+
+        if(Input.GetKey(KeyCode.Space))
         {
-            SceneManager.LoadScene("Character Select"); //씬을 Charater Select 라는 씬으로 바꿈.
+             SceneManager.LoadScene("Character Select");
         }
+    
     }
 }
