fbc75a3bd4d07f1d7e6a56ad9259f9f1d609948c
river0203 <aspp3666@gmail.com>
2023-05-16 23:09:32 +0900

    FIX : time, input

--- sf2/Assets/Scripts/Manager/InputManager.cs
+++ sf2/Assets/Scripts/Manager/InputManager.cs
@@ -7,6 +7,7 @@ public class InputManager
 {
     public Action<bool> ButtonType_check;
     bool a;
+    
     //key 입력을 bool 값으로 확인
     //확인된 정보를 다른 UI, character로 제공
     static void PressButton(bool ButtonKey) //Action 구현
--- sf2/Assets/Scripts/Manager/TimeManager.cs
+++ sf2/Assets/Scripts/Manager/TimeManager.cs
@@ -4,27 +4,29 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace PIC
+public class TimeManager : MonoBehaviour
 {
-    public class TimeManager : MonoBehaviour
+    [SerializeField]
+    public float MaxTime = 60;
+    int first_time = 0;
+    int last_time = 0;
+    int CurrentTime = 0;
+    
+    private void Start()
     {
-        public float MaxTime = 60;
-        int first_time = 0;
-        int last_time = 0;
-        int CurrentTime = 0;
-
-        private void Start()
-        {
-            CurrentTime = (int)MaxTime;
-        }
-        private void Update()
-        {
-            MaxTime -= Time.deltaTime;
-            CurrentTime = (int)MaxTime;
-            first_time = CurrentTime / 10;
-            last_time = CurrentTime % 10;
-            Debug.Log(first_time);
-            Debug.Log(last_time);
-        }    
+        CurrentTime = (int)MaxTime;
     }
+    private void Update()
+    {
+        MaxTime -= Time.deltaTime;
+        CurrentTime = (int)MaxTime;
+        first_time = CurrentTime / 10;
+        last_time = CurrentTime % 10;
+        Debug.Log(first_time);
+        Debug.Log(last_time);
+        if(CurrentTime > MaxTime)
+        {
+            
+        }
+    }    
 }
