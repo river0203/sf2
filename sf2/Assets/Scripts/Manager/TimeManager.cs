f081e04b1f23e26692a4afc78427d42cdcf85884
river0203 <aspp3666@gmail.com>
2023-05-16 22:07:19 +0900

    FEAT : TImeManager

---
+++ sf2/Assets/Scripts/Manager/TimeManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace PIC
+{
+    public class TimeManager : MonoBehaviour
+    {
+        public float MaxTime = 60;
+        int first_time = 0;
+        int last_time = 0;
+        int CurrentTime = 0;
+
+        private void Start()
+        {
+            CurrentTime = (int)MaxTime;
+        }
+        private void Update()
+        {
+            MaxTime -= Time.deltaTime;
+            CurrentTime = (int)MaxTime;
+            first_time = CurrentTime / 10;
+            last_time = CurrentTime % 10;
+            Debug.Log(first_time);
+            Debug.Log(last_time);
+        }    
+    }
+}
