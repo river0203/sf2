45e0d83c7a465807997f1b2a92a95e92ac565c2e
wonjae1267 <bjng1267@naver.com>
2023-05-18 16:48:30 +0900

    Update Game Scene

---
+++ sf2/Assets/Script/Hpbar_Right.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hpbar_Right : MonoBehaviour
+{
+    void Update()
+    {
+        PlayerHPbarRight();
+    }
+
+    public void PlayerHPbarRight()
+    {
+        /*float HP = 클래스.Instance.플레이어 HP; //캐릭터 hp를 받아오고 */
+        /*hpbar.fillamount = HP/100f; //체력 바 (UI)를 fillamount 값을 변경하여 체력 표시 */
+    }
+}
