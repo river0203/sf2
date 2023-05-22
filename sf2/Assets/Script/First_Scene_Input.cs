79514ffa1b3413f7f967c1a7f0d5cec8ce5607b7
wonjae1267 <bjng1267@naver.com>
2023-05-22 20:42:07 +0900

    Revert "FIX : First Scene Script"
    This reverts commit cd3202b790d62f43378ac720368a33399269fa39.

--- sf2/Assets/Script/First_Scene_Input.cs
+++ sf2/Assets/Script/First_Scene_Input.cs
@@ -5,17 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class First_Scene_Input : MonoBehaviour
 {
-
-    public InputManager button_bool;
-    void Start()
-    {
-        button_bool+=ButtonType_check;
-    }
    
     void Update()
     {
 
-        if(button_bool==true)
+        if(Input.GetKey(KeyCode.Space))
         {
              SceneManager.LoadScene("Character Select");
         }
