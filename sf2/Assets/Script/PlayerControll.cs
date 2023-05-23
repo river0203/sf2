5355686de72928593f7a7685f32a1788f574fee3
wonjae1267 <bjng1267@naver.com>
2023-05-23 21:40:07 +0900

    FEAT : anim

--- sf2/Assets/Script/PlayerControll.cs
+++ sf2/Assets/Script/PlayerControll.cs
@@ -8,10 +8,15 @@ public class PlayerControll : MonoBehaviour
 { /*static float offensePower = 10;
     public float get_offensePower {get {return offensePower;}}*/
 
+    Animator animator;
     [SerializeField]
     float maxSpeed = 5f;
     float moveX, moveY;
-    
+    private bool isMoving;
+    void Start()
+    {
+        animator=GetComponent<Animator>();
+    }
     private void Update() 
     {
         player_move();
@@ -21,10 +26,35 @@ public class PlayerControll : MonoBehaviour
     }private void player_move() //다시
     {
         moveX = Input.GetAxis("Horizontal") * maxSpeed * Time.deltaTime;
-        moveY = Input.GetAxis("Vertical") * maxSpeed * Time.deltaTime;
+        moveY = Input.GetAxis("Vertical") * maxSpeed * 2 *Time.deltaTime;
+
+        if(moveX>0)
+        {
+            animator.Play("RYU Walk");
+           
+        }
+        if(moveX==0)
+        {
+            animator.Play("RYU Idle");
+        }
+        if(moveX<0)
+        {
+            animator.Play("RYU Walk");
+        }
+        if(moveY<0)
+        {
+            animator.Play("RYU Sit");
+        }
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            animator.Play("RYU Jump");
+        }
+        
+       
 
         transform.position = new Vector2(transform.position.x + moveX, transform.position.y + moveY);
     }
+
 
     private void player_Nmove()
     {
@@ -34,11 +64,11 @@ public class PlayerControll : MonoBehaviour
             maxSpeed = 0.0f; //다음 라운드에서는 true로 전환되어 움직일 수 있는 코드 필요
             Debug.Log("player stop");
         }
-        else if(PlayerState.player_hp(PlayerState.HpType) == false)
+        /*else if(PlayerState.player_hp(PlayerState.HpType) == false)
         {
             maxSpeed = 0.0f;
             Debug.Log("player stop");
-        }
+        }*/
     }
 
     private void player_attack()
@@ -49,8 +79,7 @@ public class PlayerControll : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.A))
         {
-            //애니메이션 불러오기 } if(!Input.GetKey(KeyCode.A))
-        
+            //애니메이션 불러오기 
         }
     }
 }
