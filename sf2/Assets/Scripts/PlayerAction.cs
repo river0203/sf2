5bcac29c61f085f163ec10993acf54aa05178467
ppic0423 <41542733+ppic0423@users.noreply.github.com>
2023-05-24 21:58:58 +0900

    REACTOR :: PlayerAction.cs

--- sf2/Assets/Scripts/PlayerAction.cs
+++ sf2/Assets/Scripts/PlayerAction.cs
@@ -9,9 +9,29 @@ public class PlayerAction : MonoBehaviour
     Rigidbody2D rigid;
 
     [SerializeField]
-    GameObject[] AttackHitBox;
+    [Tooltip("HitBox")]
+    GameObject strong_adogen;
+    GameObject middle_adogen;
+    GameObject breaking_the_collarbone;
+    GameObject pit_of_the_stomach;
+    GameObject oryugen;
+    GameObject strong_addaddaddugen;
+    GameObject middle_addaddaddugen;
+    GameObject DownKick;
+    GameObject SitRightpunch;
+    GameObject SitLeftPunch;
+    GameObject LeftKick;
+    GameObject LeftPunch;
+    GameObject SitRightKick;
+    GameObject LeftUpperCut;
+    GameObject RightKneeKick;
+    GameObject RightPunch;
+    GameObject SitLeftKick;
+
+
     GameObject Idle_Jump_HitBox;
     GameObject SitDown_HitBox;
+    
     GameObject Adogen;
 
     [SerializeField]
@@ -29,24 +49,34 @@ public class PlayerAction : MonoBehaviour
     void Update()
     {
         IdleAction();
-        JumpAction();
-        SitDownAction();
     }
-     void IdleAction()
-     {
-         Command.Skill_set = PlayerControll.status.weak_oryugen;
-     }
-     void JumpAction()
-     {
+    void IdleAction()
+    {
+        if(Command.Skill_set == PlayerControll.status.strong_addaddaddugen)
+        {
+            strong_addaddaddugen.SetActive(true);
+        }
+        else if (Command.Skill_set == PlayerControll.status.middle_addaddaddugen)
+        {
+            middle_addaddaddugen.SetActive(true);
+        }
+        else if (Command.Skill_set == PlayerControll.status.breaking_the_collarbone)
+        {
+            breaking_the_collarbone.SetActive(true);
+        }
+        else if (Command.Skill_set == PlayerControll.status.pit_of_the_stomach)
+        {
+            pit_of_the_stomach.SetActive(true);
+        }
+        else if (Command.Skill_set == PlayerControll.status.oryugen)
+        {
+            oryugen.SetActive(true);
+        }
+    }
 
-     }
-     void SitDownAction()
-     {
-
-     }
-     IEnumerator CreateAdogen()
-     {
-         yield return new WaitForSeconds(0.7f);
-         Instantiate(Adogen, new Vector3(rigid.position.x + 1.7f, rigid.position.y + 0.77f, 0), Quaternion.identity);
-     }
- }
+    IEnumerator CreateAdogen()
+    {
+        yield return new WaitForSeconds(0.7f);
+        Instantiate(Adogen, new Vector3(rigid.position.x + 1.7f, rigid.position.y + 0.77f, 0), Quaternion.identity);
+    }
+}
